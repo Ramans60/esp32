fc856e02f724ae31b8219e8adbd6befefc0591bf
Raman s <sivanraman60@gmail.com>
2026-06-09 12:36:45 +0530

    Add files via upload

---
+++ one-one.cpp
@@ -0,0 +1,40 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(26, OUTPUT);  
+}
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(2, LOW);
+  delay(1000);
+  digitalWrite(4, HIGH);
+  digitalWrite(4, LOW);
+  delay(1000);
+  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(18, HIGH);
+  digitalWrite(18, LOW);
+  delay(1000);
+  digitalWrite(19, HIGH);
+  digitalWrite(19, LOW);
+  delay(1000);
+  digitalWrite(21, HIGH);
+  digitalWrite(21, LOW);
+  delay(1000);
+  digitalWrite(22, HIGH);
+  digitalWrite(22, LOW);
+  delay(1000);
+  digitalWrite(23, HIGH);
+  digitalWrite(23, LOW);
+  delay(1000);
+  digitalWrite(26, HIGH);
+  digitalWrite(26, LOW);
+  delay(1000);
+}
